a1cfe4dffb12a354585f45e4d2c84194138741ec
HongyiLU <adamlu389535891@gamil.com>
2021-01-11 15:57:05 +0100

    Add basic function of enterprise register

---
+++ src/main/java/com/projectagile/webprojectagile/controller/EnterpriseRegisterController.java
@@ -0,0 +1,35 @@
+package com.projectagile.webprojectagile.controller;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+import com.projectagile.webprojectagile.entity.Enterprise;
+import com.projectagile.webprojectagile.entity.TestClass;
+import com.projectagile.webprojectagile.enums.ResultEnum;
+import com.projectagile.webprojectagile.service.EnterpriseService;
+import com.projectagile.webprojectagile.utils.ResultVOUtils;
+import com.projectagile.webprojectagile.vo.req.EnterpriseRegisterReqVO;
+import com.projectagile.webprojectagile.vo.res.BaseResVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user/enterprise")
+public class EnterpriseRegisterController {
+
+    @Autowired
+    EnterpriseService enterpriseService;
+
+    @PostMapping("/register")
+    public BaseResVO enterpriseRegister(@RequestBody EnterpriseRegisterReqVO enterpriseRegisterReqVO){
+        System.out.println(enterpriseRegisterReqVO.toString());
+        Enterprise enterprise = enterpriseService.insertEnterprise(enterpriseRegisterReqVO.toEnterprise());
+        //TODO Verify data format
+        if(enterprise != null){
+            return ResultVOUtils.success(enterprise);
+        } else {
+            return ResultVOUtils.error(ResultEnum.PARAM_VERIFY_FALL);
+        }
+    }
+}
